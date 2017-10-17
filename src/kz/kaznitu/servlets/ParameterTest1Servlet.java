60e76737eba8ce5dba33ae33a7cd2c4cd028e70e
Botagoz <musabekovab98@gmail.com>
2017-10-17 16:17:18 +0600

    Bota1

--- src/kz/kaznitu/servlets/ParameterTest1Servlet.java
+++ src/kz/kaznitu/servlets/ParameterTest1Servlet.java
@@ -16,6 +16,6 @@ public class ParameterTest1Servlet extends HttpServlet {
             PrintWriter out = response.getWriter();
             String title = "Using GET Method to Read Form Data";
             String docType = "<!doctype html>\n"; out.println(docType +
-                "<html>\n"+ "<head><title>" + title +"</title><head>\n"
-                + "<body bgcolor = \"#f0f0f0\">\n"+ "<h1 align = \"center\">"+ title +"</h1\n>"+ "<ul>\n"+ "<li><b>First Name</b>:" +request.getParameter("first_name")+"\n"+ "<li><b>Last Name</b>:" +request.getParameter("last_name")+"\n"+ "</ul>\n"+ "</body>\n"+ "</html>" );
+                "<html>\n"+ "<head><title>"  + title +"</title><head>\n"
+                + "<body bgcolor = \"#f0f0f0\">\n" + "<h1 align = \"center\">" + title + "</h1\n>" + "<ul>\n" + "<li><b>First Name</b>:" +request.getParameter("first_name")+"\n"+ "<li><b>Last Name</b>:" +request.getParameter("last_name")+"\n"+ "</ul>\n"+ "</body>\n"+ "</html>" );
     } }
